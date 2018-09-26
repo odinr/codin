084d43334207a9e244b9c3f2151f51e2ddfb3995
Odin Rochmman <odin.rochmann@gmail.com>
2018-09-26 08:00:11 +0200

    chore()
    moved to WIP

--- packages/elements/picture/lib/lazy-data.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const observer: IntersectionObserver | {
-    observe: (target: Element) => boolean;
-};
-export declare function observe(element: Element, attributes: string[]): void;
-export default observe;
