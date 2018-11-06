87c4cff050ed6e97a6dd160cb220dbd76e8f1193
Odin Rochmman <odin.rochmann@gmail.com>
2018-11-06 07:37:56 +0100

    doc(ts)

--- utils/event-observer/build/event-observer.d.ts
+++ utils/event-observer/build/event-observer.d.ts
@@ -1,6 +1,11 @@
 export interface EventObserverCallback<T extends Event> {
     (event: T, observer: EventObserver<T>): void;
 }
+/**
+ * Wrapper class for mapping events to a callback.
+ * Observe and unobserve without redefining callback.
+ * Hold reference to callback.
+ */
 export declare class EventObserver<T extends Event> {
     protected _cb: EventObserverCallback<T>;
     protected _opt: {
@@ -10,8 +15,23 @@ export declare class EventObserver<T extends Event> {
     constructor(_cb: EventObserverCallback<T>, _opt?: {
         passive: boolean;
     });
+    /**
+     * Observers an element type of event
+     * @param element element to observe
+     * @param type type of event to observe
+     */
     observe(element: Element, type: string): void;
-    unobserve(element: Element, type: string): void;
+    /**
+     * Stops listening for event on element.
+     * If no type provided, all registered events are removed.
+     * @param element element to unregister event listening
+     * @param type type of event to stop listen for
+     */
+    unobserve(element: Element, type?: string): void;
+    /**
+     * Internal proxy for callbacks.
+     * @param ev Event
+     */
     handleEvent(ev: T): void;
 }
 export default EventObserver;
--- utils/event-observer/src/build/event-observer.ts
+++ utils/event-observer/src/build/event-observer.ts
@@ -2,15 +2,24 @@ export interface EventObserverCallback<T extends Event> {
  (event: T, observer: EventObserver<T>): void;
 }
 
+/**
+ * Wrapper class for mapping events to a callback.
+ * Observe and unobserve without redefining callback.
+ * Hold reference to callback.
+ */
 export class EventObserver<T extends Event> {
  protected _observationTargets: Map<Element, string[]> = new Map();
-
  constructor(
   protected _cb: EventObserverCallback<T>,
   protected _opt = { passive: true }
  ) {}
 
- observe(element: Element, type: string) {
+ /**
+  * Observers an element type of event
+  * @param element element to observe
+  * @param type type of event to observe
+  */
+ public observe(element: Element, type: string) {
      const targetTypes = this._observationTargets.get(element) || [];
      targetTypes.includes(type) || (() => {
          element.addEventListener(type, this, this._opt);
@@ -18,7 +27,13 @@ export class EventObserver<T extends Event> {
      })();
  }
 
- unobserve(element: Element, type: string) {
+ /**
+  * Stops listening for event on element.
+  * If no type provided, all registered events are removed.
+  * @param element element to unregister event listening
+  * @param type type of event to stop listen for
+  */
+ public unobserve(element: Element, type?: string) {
      const targetTypes = this._observationTargets.get(element) || [];
      const removeTypes = type && targetTypes.includes(type) ? [type] : (type ? [] : targetTypes);
      const remainTypes = targetTypes.filter((t) => !removeTypes.includes(t));
@@ -26,7 +41,11 @@ export class EventObserver<T extends Event> {
      remainTypes.length > 0 ? this._observationTargets.set(element, remainTypes) : this._observationTargets.delete(element);
  }
 
- handleEvent(ev: T) {
+ /**
+  * Internal proxy for callbacks.
+  * @param ev Event
+  */
+ public handleEvent(ev: T) {
      this._cb(ev, this);
  }
 }
