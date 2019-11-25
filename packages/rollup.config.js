bf614ca821b2b7fb5b1069a2c8bebfbc1e3295ed
odin thomas rochmann <odin.rochmann@gmail.com>
2019-11-25 20:46:56 +0100

    chore: upgraded packages

--- packages/rollup.config.js
+++ packages/rollup.config.js
@@ -1,35 +1,25 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import preset from '@babel/preset-env';
-import {terser} from 'rollup-plugin-terser';
-import camelCase from 'lodash.camelcase';
-import progress from 'rollup-plugin-progress';
-import filesize from 'rollup-plugin-filesize';
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import preset from "@babel/preset-env";
+import { terser } from "rollup-plugin-terser";
+import camelCase from "lodash.camelcase";
+import progress from "rollup-plugin-progress";
+import filesize from "rollup-plugin-filesize";
 
 const pkg = require(`${process.cwd()}/package.json`);
-const name = camelCase(pkg.name);
-function create(args) {
-  const {module, ...rest} = args;
-  const format = 'esm';
-  const plugins = [
-    progress({clearline:false}),
-    filesize({showMinifiedSize: false}),
-    babel({presets: [[preset, {targets: {esmodules: true}}]]}),
-    terser(),
-  ];
-  return {
-    input: pkg.main,
-    output: {
-      name,
-      format,
-      file: module ? pkg.module : pkg.browser,
-    },
-    plugins: module ? plugins : [resolve(), ...plugins],
-    ...rest
-  };
-}
 
-export default [
-  create({module: true}),
-  create({module: false}),
-];
+export default {
+  input: pkg.main,
+  output: {
+    name: camelCase(pkg.name),
+    format: "esm",
+    file: pkg.browser
+  },
+  plugins: [
+    progress({ clearline: false }),
+    filesize({ showMinifiedSize: false }),
+    resolve(),
+    babel({ presets: [[preset, { targets: { esmodules: true } }]] }),
+    terser()
+  ]
+};
